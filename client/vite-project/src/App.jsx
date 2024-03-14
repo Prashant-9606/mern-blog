cbc4f358c26923df718372a6d7a4a00fab4a439d
Prashant-9606 <prashant060200@gmail.com>
2024-03-14 19:08:51 +0530

    create pages

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,9 +1,22 @@
-
+import { BrowserRouter as Router, 
+         Routes,
+         Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl text-red-500">App Page</h1>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/signin' element={<SignIn/>}/>
+          <Route exact path='/signup' element={<SignUp/>}/>
+          <Route exact path='/projects' element={<Projects/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
