1cd639ea32257a6bf3095eec1f244cb00f05a27d
Prashant-9606 <prashant060200@gmail.com>
2024-03-15 19:14:28 +0530

    designed the header section

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -5,16 +5,20 @@ import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
+import About from './pages/About'
+import Header from './components/Header'
 function App() {
 
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route exact path='/' element={<Home/>}/>
           <Route exact path='/signin' element={<SignIn/>}/>
           <Route exact path='/signup' element={<SignUp/>}/>
           <Route exact path='/projects' element={<Projects/>}/>
+          <Route exact path='/about' element={<About/>}/>
         </Routes>
       </Router>
     </>
